98150bd2e5b0bab719d74ded8b6c24b61851f156
GCSebastian <juan.gaiborc.est@uets.edu.ec>
2024-02-17 15:35:05 -0500

    CompletoMovil
    Ya es posible desplegar con el movil

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDoRz1GT8Ew61oVTu4Kkyih_tu7WGcQYdQ",
+    authDomain: "iotpagejsgc.firebaseapp.com",
+    projectId: "iotpagejsgc",
+    storageBucket: "iotpagejsgc.appspot.com",
+    messagingSenderId: "1070599915019",
+    appId: "1:1070599915019:web:2a62d1f9b5a439126de90e"
+  }
 };
